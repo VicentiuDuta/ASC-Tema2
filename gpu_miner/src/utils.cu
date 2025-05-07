ea42bd3b94d83ff9c45f5d7f43c23f6cd6832b4c
VicentiuDuta <dutavicentiualecsandru@yahoo.com>
2025-05-07 19:08:40 +0300

    Implemented merkle_root

--- gpu_miner/src/utils.cu
+++ gpu_miner/src/utils.cu
@@ -88,6 +88,49 @@ __host__ __device__ int compare_hashes(BYTE* hash1, BYTE* hash2) {
     }
     return 0; // hashes are equal
 }
+__global__ void compute_transaction_hashes_kernel(BYTE *transactions, BYTE *hashes, int transaction_size, int n) {
+    unsigned int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    if (idx < n) {
+        apply_sha256(transactions + idx * transaction_size, hashes + idx * SHA256_HASH_SIZE);
+    }
+}
+
+__global__ void construct_merkle_level_kernel(BYTE *hashes, int n, BYTE *device_next_hashes) {
+        // Allocate shared memory for the current level
+        __shared__ BYTE shared_hashes[256 * SHA256_HASH_SIZE];
+        unsigned int tid = threadIdx.x;
+        unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+        // Load the hashes into shared memory 
+        if (idx < n) {
+            for (int i = 0; i < SHA256_HASH_SIZE; i++) {
+                shared_hashes[tid * SHA256_HASH_SIZE + i] = hashes[idx * SHA256_HASH_SIZE + i];
+            }
+        } 
+        
+        // Synchronize threads to ensure all data is loaded
+        __syncthreads();
+        
+        // Compute the next level of hashes only with the first half of threads
+        if (idx < n / 2) {
+            BYTE combined_hash[2 * SHA256_HASH_SIZE];
+            int idx1 = tid * 2;
+            int idx2 = tid * 2 + 1;
+
+            // Copy the first hash
+            d_strcpy((char *) combined_hash, (const char *) (shared_hashes + idx1 * SHA256_HASH_SIZE));
+            // If the second hash exists, concatenate it
+            if (idx2 < blockDim.x && idx * 2 + 1 < n) {
+                d_strcat((char *) combined_hash, (const char *) (shared_hashes + idx2 * SHA256_HASH_SIZE));
+            } else {
+                // If the second hash does not exist, duplicate the first hash
+                d_strcat((char *) combined_hash, (const char *) (shared_hashes + idx1 * SHA256_HASH_SIZE));
+            }
+            
+            // Compute the hash of the combined hashes
+            apply_sha256((BYTE *) combined_hash, device_next_hashes + idx * SHA256_HASH_SIZE);
+        }
+} 
 
 // TODO 1: Implement this function in CUDA
 void construct_merkle_root(int transaction_size, BYTE *transactions, int max_transactions_in_a_block, int n, BYTE merkle_root[SHA256_HASH_SIZE]) {
@@ -119,28 +162,74 @@ void construct_merkle_root(int transaction_size, BYTE *transactions, int max_tra
     // Free device memory
     cudaFree(device_transactions);
     
-    // Allocate device memory for merkle root
-    BYTE *device_merkle_root;
-    cudaMalloc((void **) &device_merkle_root, SHA256_HASH_SIZE);
+    // Allocate device memory for next level hashes
+    BYTE *device_next_hashes;
+    cudaMalloc((void **) &device_next_hashes, (n / 2 + n % 2) * SHA256_HASH_SIZE);
+
+    while (n > 1) {
+        int new_n = n / 2;
+        if (n % 2 != 0)
+            new_n++; // if odd, duplicate last hash
+
+        num_blocks = n / block_size;
+        if(n % block_size != 0) {
+            num_blocks++; // round up
+        }
+
+        construct_merkle_level_kernel<<<num_blocks, block_size>>>(device_hashes, n, device_next_hashes);
+        cudaDeviceSynchronize();
+
+        // Copy the next level hashes to the device
+        BYTE *temp = device_hashes;
+        device_hashes = device_next_hashes;
+        device_next_hashes = temp;
+        n = new_n;
+    }
+
+    // Copy the final merkle root to host
+    cudaMemcpy(merkle_root, device_hashes, SHA256_HASH_SIZE, cudaMemcpyDeviceToHost);
+    // Free device memory
+    cudaFree(device_hashes);
+    cudaFree(device_next_hashes);
+    // Free host memory
+    free(hashes);
+
+}
+
+void find_nonce_kernel(BYTE *difficulty, uint32_t max_nonce, BYTE *block_content, size_t current_length, BYTE *block_hash, uint32_t *valid_nonce, uint32_t *found_nonce_flag) {
 
 }
 
 // TODO 2: Implement this function in CUDA
 int find_nonce(BYTE *difficulty, uint32_t max_nonce, BYTE *block_content, size_t current_length, BYTE *block_hash, uint32_t *valid_nonce) {
-    
+    // Allocate device memory for block content
+    BYTE *device_difficulty;
+    cudaMalloc((void **) &device_difficulty, SHA256_HASH_SIZE);
+    cudaMemcpy(device_difficulty, difficulty, SHA256_HASH_SIZE, cudaMemcpyHostToDevice);
+
+    BYTE *device_block_content;
+    cudaMalloc((void **) &device_block_content, current_length);
+    cudaMemcpy(device_block_content, block_content, current_length, cudaMemcpyHostToDevice);
+
+    BYTE *device_block_hash;
+    cudaMalloc((void **) &device_block_hash, SHA256_HASH_SIZE);
+    cudaMemcpy(device_block_hash, block_hash, SHA256_HASH_SIZE, cudaMemcpyHostToDevice);
+
+    uint32_t *device_valid_nonce;
+    cudaMalloc((void **) &device_valid_nonce, sizeof(uint32_t));
+
+    int* device_found_nonce;
+    cudaMalloc((void **) &device_found_nonce, sizeof(int));
+    cudaMemset(device_found_nonce, 0, sizeof(int));
+
+    // Declare kernel parameters
+    const size_t block_size = 512;
+    size_t num_blocks = max_nonce / block_size;
+    if (max_nonce % block_size != 0) {
+        num_blocks++;
+    }
 }
 
-__global__ void compute_transaction_hashes_kernel(BYTE *transactions, BYTE *hashes, int transaction_size, int n) {
-    unsigned int idx = threadIdx.x + blockDim.x * blockIdx.x;
-    if (idx < n) [
-        apply_sha256(transactions + idx * transaction_size, hashes[idx]);
-    ]
-}
-
-__global__ void construct_merkle_root_kernel(BYTE *hashes, int n, BYTE *merkle_root) {
-    unsigned int idx = threadIdx.x + blockDim.x * blockIdx.x;
-    
-} 
 
 __global__ void dummy_kernel() {}
 
